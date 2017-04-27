694e6cd55e575bf62110fbcd81c4f8c412ea9aaf
Nils_Hamacher <hamacher.nils@gmail.com>
2017-04-27 22:25:50 +0200

    added some function to get the signal of the WebCam of the Laptop but only commented out because of driver issues with windows there is a problem to receive a camera signal.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,11 +2,15 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+  // frame in which the camera signal will be shown
+  //cam.setup(320, 240);
 
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+  // stetic update of the received Signal
+  //cam.update();
 
 }
 
@@ -14,7 +18,10 @@ void ofApp::update(){
 // Draw a red circle that's center is at the position of the Cursor
 void ofApp::draw() {
   ofSetColor(255, 0, 0);
-  ofDrawSphere(ofGetMouseX(), ofGetMouseY(), 50);
+  ofDrawSphere(500, 500, 1);
+
+  // finaly draw the camera frame 
+  //cam.draw(0, 0);
 }
 
 //--------------------------------------------------------------
@@ -59,6 +66,7 @@ void ofApp::mouseExited(int x, int y){
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
+
 
 }
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -21,4 +21,7 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 		
+
+    // Object for camera signal
+    //ofVideoGrabber cam;
 };
