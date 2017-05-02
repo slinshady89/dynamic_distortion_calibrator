3cc3a196067f4cdbc85bec06864e6135dd6713ad
Nils_Hamacher <hamacher.nils@gmail.com>
2017-05-02 23:21:18 +0200

    copying webcam image into another

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,153 +1,150 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
-  // frame in which the camera signal will be shown
-  cam.setup(320,240);
-  ofBackground(ofColor::white);
-  //set framerate (speed) of the travelling pixel
-  ofSetFrameRate(240);
-  windowHeight = ofGetWindowHeight();
-  windowWidth = ofGetWindowWidth();
-  startX = round(windowWidth / 2.0);
-  startY = round(windowHeight / 2.0);
-  x = startX;
-  y = startY;
-  pixelSize = 10;
-  spiralSize = pixelSize;
-  spiralDirection = 0;
+void ofApp::setup() {
+	// frame in which the camera signal will be shown
+	cam.setup(320, 240);
+	ofBackground(ofColor::white);
+	//set framerate (speed) of the travelling pixel
+	ofSetFrameRate(500);
+	windowHeight = ofGetWindowHeight();
+	windowWidth = ofGetWindowWidth();
+	startX = floor(windowWidth / 2.0);
+	startY = floor(windowHeight / 2.0);
+	x = startX;
+	y = startY;
+	pixelSize = 10;
+	spiralSize = pixelSize;
+	spiralDirection = 0;
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
-  // stetic update of the received Signal
-  cam.update();
-  //cam.getPixels();
+void ofApp::update() {
+	// stetic update of the received Signal
+	cam.update();
+	colorImage = cam.getPixels();
+  grayImage.clone(colorImage.setImageType(OF_IMAGE_GRAYSCALE));
 
-
-  // update the coordinates of the pixel with every new frame
-  /*x += 1;
-  if (ofGetWindowWidth() < x)
-  {
-    x = 0;
-    y += 1;
-  }
-  if (ofGetWindowHeight() < y && ofGetWindowWidth() < x) {
-    x = 0;
-    y = 0;
-  }*/
-
-  // attempting to spiral pixel from middle point
-  switch (spiralDirection) {
+	// attempting to spiral pixel from middle point
+	switch (spiralDirection) {
 	case 0: std::cout << "in case 0\n";
+		if (y < 0) {                        // reaching upper windowborder
+			x = startX - spiralSize;          // change x to left spiral side
+			spiralDirection = 2;              // -> 180° turn of direction
+		}
 		y--;
-		if (y == startY - spiralSize) {
-			spiralDirection = 1; // change direction to left
+		if (y <= startY - spiralSize) {     // dy > spiralsize will lead to 
+			spiralDirection = 1;              // change of direction to left
 		};
 		break;
 	case 1: std::cout << "in case 1\n";
+		if (x < 0) {                        // reaching left windowborder
+			y = startY + spiralSize;          // change x to upper spiral side
+			spiralDirection = 3;              // -> 180° turn of direction
+		}
 		x--;
-		if (x == startX - spiralSize) {
-			spiralDirection = 2; // change direction to left
+		if (x <= startX - spiralSize) {     // dx < spiralsize will lead to 
+			spiralDirection = 2;              // change of direction to left
 			spiralSize += pixelSize;
 		};
 		break;
 	case 2: std::cout << "in case 2\n";
-		  y++;
-		  if (y == startY + spiralSize) {
-			  spiralDirection = 3; // change direction to left
-		  };
-		  break;
+		if (y > ofGetWindowHeight()) {
+			x = startX + spiralSize;
+			spiralDirection = 0;
+		}
+		y++;
+		if (y >= startY + spiralSize) {
+			spiralDirection = 3; // change direction to left
+		};
+		break;
 	case 3: std::cout << "in case 3\n";
-		  x++;
-		  if (x == startX + spiralSize) {
-			  spiralDirection = 0; // change direction to left
-			  spiralSize += pixelSize;
-		  };
-		  break;
+		if (x > ofGetWindowWidth()) {
+			y = startY - spiralSize;
+			spiralDirection = 1;
+		}
+		x++;
+		if (x >= startX + spiralSize) {
+			spiralDirection = 0; // change direction to left
+			spiralSize += pixelSize;
+		};
+		break;
 	}
-
 }
 
 //--------------------------------------------------------------
 // Make a white plane on which one or more Pixel(s) are travelling 
 void ofApp::draw() {
+	// finaly draw the camera frame 
+	colorImage.draw(0, 0);
+  grayImage.draw(400, 340);
+	// get screen resolution
+	// horizon = ofGetWindowWidth();
+	// vert = ofGetWindowHeight();
 
-  //// make white plane
-  //ofSetColor(255, 255, 255);
-  //ofDrawRectangle(0, 0, 1920, 1080);
+	// output of screen resolution for testing
+	//string text = "width " + to_string(horizon) + " height " + to_string(vert);
+	//ofDrawBitmapStringHighlight(text,ofPoint(10,10,0.0),ofColor::white,ofColor::black );
 
-  // get screen resolution
-  //int horizon = ofGetWindowWidth();
-  // vert = ofGetWindowHeight();
-
-  // output of screen resolution for testing
-  //string text = "width " + to_string(horizon) + " height " + to_string(vert);
-  //ofDrawBitmapStringHighlight(text,ofPoint(10,10,0.0),ofColor::white,ofColor::black );
-
-  // draw the travelling pixel
-  ofSetColor(ofColor::red);
-  ofDrawRectangle(x, y, pixelSize, pixelSize);
-
-  string text = "breite" + to_string(cam.getWidth()) + "hoehe" + to_string(cam.getHeight());
-  ofDrawBitmapStringHighlight(text,ofPoint(10,10,0.0),ofColor::white,ofColor::black );
-
-  // finaly draw the camera frame 
-  //cam.draw(0, 0);
-}
-
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+	// draw the travelling pixel
+	ofSetColor(ofColor::black);
+	ofDrawRectangle(x, y, pixelSize, pixelSize);
+	ofSetColor(ofColor::white);
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::keyPressed(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+void ofApp::mouseExited(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
 
 
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
+void ofApp::gotMessage(ofMessage msg) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
