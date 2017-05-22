f71d24fb55271f4a8a06d683a43a1044805b7db3
Ahmed Ashraf <mtedevil@icloud.com>
2017-05-22 20:57:23 +0200

    detectingObjects   ->  only Moving?   ->  which part of the screen is important?
    committed by Nils Hamacher for the Team

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include "ofMain.h"
 #include "ofApp.h"
+#include "detectingObjects.h"
 
 //========================================================================
 int main( ){
 
 
 
-  ofSetupOpenGL(1920 , 1080, OF_WINDOW);
+  ofSetupOpenGL(1024 , 768, OF_FULLSCREEN);
+
 
   //ofSetupOpenGL(1920, 1080, OF_WINDOW);// <-------- setup the GL context
 
@@ -21,6 +23,8 @@ int main( ){
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
-	ofRunApp(new ofApp());
+	//ofRunApp(new ofApp());
+
+  ofRunApp(new detectingObjects());
 
 }
