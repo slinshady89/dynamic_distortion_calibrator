5eef4d37c1628554e21d37588c4e3c984e17281e
Nils_Hamacher <hamacher.nils@gmail.com>
2017-05-30 21:22:56 +0200

    now detects the brightest dot of the filmed frame x and y position on the frame are known

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main( ){
 
 
 
-  ofSetupOpenGL(1024 , 768, OF_FULLSCREEN);
+  ofSetupOpenGL(1024 , 768, OF_WINDOW);
 
 
   //ofSetupOpenGL(1920, 1080, OF_WINDOW);// <-------- setup the GL context
@@ -23,8 +23,9 @@ int main( ){
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
-	//ofRunApp(new ofApp());
+	ofRunApp(new ofApp());
 
-  ofRunApp(new detectingObjects());
+  //ofRunApp(new detectingObjects());
 
 }
+
